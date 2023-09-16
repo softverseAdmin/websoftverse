1eb89b6ca57cc04aa357d64202348835c59d11aa
sudip khatri <sudpkhatri4@gmail.com>
2023-09-16 12:41:48 +1000

    verAus: home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Main from './components/main/Main';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="header">
+      </div>
+      <div className="main">
+        <Main />
+      </div>
+      <div className="footer">
+      </div>
     </div>
   );
 }
